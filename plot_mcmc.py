2d4265c102673fd64272938652e1dd2be5fe2c2d
shivesh <s.p.mandalia@qmul.ac.uk>
2017-09-19 15:53:42 -0500

    Tue 19 Sep 15:53:42 CDT 2017

--- plot_mcmc.py
+++ plot_mcmc.py
@@ -11,8 +11,34 @@ import numpy as np
 import matplotlib
 import corner
 
-print 'Making plot'
-samples = np.load('/data/mandalia/flavour_ratio/data/mcmc_chain.npy')
-fig = corner.corner(samples)
-fig.savefig("untitled.png")
-print 'DONE!'
+
+def parse_args():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '--chains', type=str, required=True,
+        help='Path to chains'
+    )
+    parser.add_argument(
+        '--outfile', type=str, default='./untitled.npy',
+        help='Path to output chains'
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    print 'Making plot'
+    samples = np.load(args.chains)
+    fig = corner.corner(samples)
+    fig.savefig(args.outfile)
+    print 'DONE!'
+
+
+main.__doc__ = __doc__
+
+
+if __name__ == '__main__':
+    main()
