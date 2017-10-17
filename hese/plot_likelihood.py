e4041638c938f3da42e65ab774d1b8434f60269b
shivesh <s.p.mandalia@qmul.ac.uk>
2017-10-17 02:45:20 +0100

    Tue Oct 17 02:45:20 BST 2017

--- hese/plot_likelihood.py
+++ hese/plot_likelihood.py
@@ -6,7 +6,7 @@ from scipy.stats import multivariate_normal
 
 
 BESTFIT=[1, 1, 1]
-SIGMA=0.2
+SIGMA=0.20
 
 def cartesian(arrays, out=None):
     arrays = [np.asarray(x) for x in arrays]
@@ -38,7 +38,7 @@ print p.shape
 p = p[p[:,-1] > 0]
 
 cov_fr = np.identity(3) * SIGMA
-llh = np.log10(multivariate_normal.pdf(p, mean=BESTFIT, cov=cov_fr))
+llh = -np.log10(multivariate_normal.pdf(p, mean=BESTFIT, cov=cov_fr))
 print llh
 print llh.shape
 
